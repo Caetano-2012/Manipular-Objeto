8e2b7efb4effa3ec2ac627bc15ca77f6b3becd01
Caetano Bordin <hyperplayer2012@gmail.com>
2025-11-10 14:55:08 -0300

    Finalização do projeto

--- main.js
+++ main.js
@@ -10,7 +10,35 @@ let poseNet;
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
+    video.position(350, 150);
 
     canvas = createCanvas(550, 550);
-    canvas.position(560, 150)
+    canvas.position(1000, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet foi inicializado!');
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+
+        difference = floor(leftWristX - rightWristX);
+    }
+}
+
+function draw() {
+    background('#969A97');
+    document.getElementById("squareSide").innerHTML = "A largura e altura do quadrado será: " + difference +"px";
+    fill('rgba(0, 150, 187, 1)');
+    stroke('rgba(0, 150, 187, 1)');
+    square(noseX, noseY, difference);
 }
